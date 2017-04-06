513c6f970adec11446367d7fd456c7046df7f808
salmanfazal01 <rustamas@uni.coventry.ac.uk>
2017-04-06 12:33:44 +0100

    Add files via upload

--- SphereBooking/src/sample/Main.java
+++ SphereBooking/src/sample/Main.java
@@ -5,14 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("template.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login/login.fxml"));
         primaryStage.setTitle("Sphere Booking");
-        primaryStage.setScene(new Scene(root, 1050, 600));
+        primaryStage.setScene(new Scene(root, 367, 354));
+        primaryStage.setResizable(false);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        //primaryStage.setScene(new Scene(root, 1080, 680));
 
         primaryStage.show();
     }
